d3f32c68c9cb0866c13850d8090445f3216c559a
mateuszanella <mateusblk1@gmail.com.br>
2024-01-30 10:09:12 -0300

    Added throttle attribute to kernel

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -40,9 +40,9 @@ class Kernel extends HttpKernel
         ],
 
         'api' => [
-            //'auth:api',
             \Illuminate\Routing\Middleware\ThrottleRequests::class.':api',
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
+            'throttle:300,1',
         ],
     ];
 
