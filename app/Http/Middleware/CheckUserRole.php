91c23d928397f4cf30ab9518a99de6257fc96ab7
mateuszanella <mateusblk1@gmail.com.br>
2024-02-07 14:06:50 -0300

    Added middleware to verify user role on api calls

---
+++ app/Http/Middleware/CheckUserRole.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class CheckUserRole
+{
+    public function handle(Request $request, Closure $next, $role)
+    {
+        if (!Auth::check() || Auth::user()->role != $role) {
+            return response()->json(['error' => 'Não autorizado'], 403);
+        }
+
+        return $next($request);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -31,35 +31,38 @@ Route::middleware('auth:api')->group(function () {
         Route::post('/punchClock', [ClockController::class, 'registerClock']);
         Route::post('/userEntries', [ClockController::class, 'getClockReport']);
         Route::post('/ticket', [TicketController::class, 'createTicket']);
+        
+        Route::prefix('setDayOff')->group(function () {
+            Route::post('/', [ClockController::class, 'setDayOffForDate']);
+        });
+    
     });
 
-    Route::prefix('admin')->group(function () {
-        Route::prefix('userEntries')->group(function () {
-            Route::get('/deleted', [ClockController::class, 'getDeletedClockEntries']);
-            Route::get('/{id}', [ClockController::class, 'getAllUserClockEntries']);
-            Route::post('/', [ClockController::class, 'insertClockEntry']);
-            Route::put('/', [ClockController::class, 'updateClockEntry']);
-            Route::delete('/{id}', [ClockController::class, 'deleteClockEntry']);
-
-            Route::prefix('setDayOff')->group(function () {
-                Route::post('/', [ClockController::class, 'setDayOffForDate']);
+    Route::middleware('role:admin')->group(function (){
+        Route::prefix('admin')->group(function () {
+            Route::prefix('userEntries')->group(function () {
+                Route::get('/deleted', [ClockController::class, 'getDeletedClockEntries']);
+                Route::get('/{id}', [ClockController::class, 'getAllUserClockEntries']);
+                Route::post('/', [ClockController::class, 'insertClockEntry']);
+                Route::put('/', [ClockController::class, 'updateClockEntry']);
+                Route::delete('/{id}', [ClockController::class, 'deleteClockEntry']);
             });
-        });
-
-        Route::prefix('manageUsers')->group(function () {
-            Route::get('/', [UserController::class, 'getAllUsers']);
-            Route::get('/user/{id}', [UserController::class, 'getUserById']);
-            Route::post('/user', [UserController::class, 'insertUser']);
-            Route::put('/user/', [UserController::class, 'updateUser']);
-            Route::delete('/user/{id}', [UserController::class, 'deleteUser']);
-            Route::get('/user/status/{id}', [UserController::class, 'checkUserStatus']);
-        });
-
-        Route::prefix('manageTickets')->group(function () {
-            Route::get('/', [TicketController::class, 'showAllTickets']);
-            Route::get('/active', [TicketController::class, 'showAllActiveTickets']);
-            Route::get('/user/{id}', [TicketController::class, 'showAllUserTickets']);
-            Route::put('/handle', [TicketController::class, 'handleTicket']);
+    
+            Route::prefix('manageUsers')->group(function () {
+                Route::get('/', [UserController::class, 'getAllUsers']);
+                Route::get('/user/{id}', [UserController::class, 'getUserById']);
+                Route::post('/user', [UserController::class, 'insertUser']);
+                Route::put('/user/', [UserController::class, 'updateUser']);
+                Route::delete('/user/{id}', [UserController::class, 'deleteUser']);
+                Route::get('/user/status/{id}', [UserController::class, 'checkUserStatus']);
+            });
+    
+            Route::prefix('manageTickets')->group(function () {
+                Route::get('/', [TicketController::class, 'showAllTickets']);
+                Route::get('/active', [TicketController::class, 'showAllActiveTickets']);
+                Route::get('/user/{id}', [TicketController::class, 'showAllUserTickets']);
+                Route::put('/handle', [TicketController::class, 'handleTicket']);
+            });
         });
     });
 
